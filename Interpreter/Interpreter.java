baa1dc7a33030740ff847baa2ed9dc5d83807eff
Shumin <33770924+shuminchen@users.noreply.github.com>
2021-01-16 20:58:19 -0500

    Add files via upload

---
+++ Interpreter/Interpreter.java
@@ -0,0 +1,21 @@
+package edu.brandeis.cs12b.pa11.interpreter;
+
+import java.util.List;
+import java.util.Map;
+
+import edu.brandeis.cs12b.pa11.parser.ParseTreeNode;
+
+public class Interpreter {
+
+	
+	public Interpreter(List<ParseTreeNode> ptns) {
+		
+	}
+
+	public Map<String, Double> evaluate() {
+		
+		return null;
+	}
+
+	
+}
